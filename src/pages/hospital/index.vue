dfa229d9a5f48a854f3df94d34b37e3f67ee2bfd
saber <lhc@saber.com>
2025-03-26 17:30:29 +0800

    Update index.vue

--- src/pages/hospital/index.vue
+++ src/pages/hospital/index.vue
@@ -1,27 +1,84 @@
 <template>
-    <div> 
-        我是医院详情001
-        第二次
-
-        第三次
-        第四次vscode
-
-        第五次   方法使用分支上传
-
-
-        我是第六次提交
-
+    <div class="hospital">
+        <!-- 左侧导航区 -->
+        <div class="menu">
+            <div class="top">
+                <el-icon>
+                    <HomeFilled />
+                </el-icon>
+                <span> / 医院信息</span>
+            </div>
+            <el-menu :default-active="$route.path" class="el-menu-vertical-demo">
+                <el-menu-item index="/hospital/register" @click="changeActive('/hospital/register')">
+                    <!-- 图标 -->
+                    <el-icon><icon-menu /></el-icon>
+                    <!-- 文字 -->
+                    <span>预约挂号</span>
+                </el-menu-item>
+                <el-menu-item index="/hospital/detail" @click="changeActive('/hospital/detail')">
+                    <el-icon>
+                        <document />
+                    </el-icon>
+                    <span>医院详情</span>
+                </el-menu-item>
+                <el-menu-item index="/hospital/notice" @click="changeActive('/hospital/notice')">
+                    <el-icon>
+                        <setting />
+                    </el-icon>
+                    <span>预约通知</span>
+                </el-menu-item>
+                <el-menu-item index="/hospital/close" @click="changeActive('/hospital/close')">
+                    <el-icon>
+                        <InfoFilled />
+                    </el-icon>
+                    <span>停诊信息</span>
+                </el-menu-item>
+                <el-menu-item index="/hospital/search" @click="changeActive('/hospital/search')">
+                    <el-icon>
+                        <Search />
+                    </el-icon>
+                    <span>查询/取消</span>
+                </el-menu-item>
+            </el-menu>
+        </div>
+        <!-- 右侧详情 -->
+        <div class="content">
+            <router-view/>
+        </div>
     </div>
 </template>
 
-<script setup >
-
+<script setup>
+import {
+  Document,
+  Menu as IconMenu,
+  Setting,
+  InfoFilled,
+  Search,
+  HomeFilled,
+} from "@element-plus/icons-vue";
 
 
 
 </script>
 
-<style lang=' ' scoped> 
+<style lang='scss' scoped>
+.hospital {
+    display: flex;
 
+    .menu {
+        flex: 2;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
 
+        .top {
+            color: #7f7f7f;
+        }
+    }
+
+    .content {
+        flex: 8;
+    }
+}
 </style>
