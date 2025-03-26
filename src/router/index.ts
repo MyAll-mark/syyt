be1a80c10c288b0783d033e6852efcaf289cc873
saber <lhc@saber.com>
2025-03-26 17:30:42 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,81 @@
-import { createRouter,createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 //createRouter方法,用于创建路由器实例，可以管理多个路由
 export default createRouter({
-    history:createWebHistory(),
+    history: createWebHistory(),
     //管理路由
-    routes:[
+    routes: [
         {
-            path:'/home',
-            component:()=>import('@/pages/home/index.vue')
+            path: '/home',
+            component: () => import('@/pages/home/index.vue')
         },
         {
-            path:'/hospital',
-            component:()=>import('@/pages/hospital/index.vue')
+            path: '/hospital',
+            component: () => import('@/pages/hospital/index.vue'),
+            children: [
+                {
+                    path: 'register',
+                    component: () => import('@/pages/hospital/register/index.vue'),
+                    meta: {
+                        title: '预约挂号'
+                    }
+                },
+                {
+                    path: 'detail',
+                    component: () => import('@/pages/hospital/detail/index.vue'),
+                    meta: {
+                        title: '医院详情'
+                    }
+                },
+                {
+                    path: 'notice',
+                    component: () => import('@/pages/hospital/notice/index.vue'),
+                    meta: {
+                        title: '预约通知'
+                    }
+                },
+                {
+                    path: 'close',
+                    component: () => import('@/pages/hospital/close/index.vue'),
+                    meta: {
+                        title: '停诊信息'
+                    }
+                },
+                {
+                    path: 'search',
+                    component: () => import('@/pages/hospital/search/index.vue'),
+                    meta: {
+                        title: '查询'
+                    }
+                },
+                // {
+                //     path: 'register_step1',
+                //     component: () => import('@/pages/hospital/register/register_step1.vue'),
+                //     meta: {
+                //         title: '预约第一步'
+                //     }
+                // },
+                // {
+                //     path: 'register_step2',
+                //     component: () => import('@/pages/hospital/register/register_step2.vue'),
+                //     meta: {
+                //         title: '预约第二步'
+                //     }
+                // }
+            ]
         },
         {
-            path:'/',
-            redirect:'/home'
+            path: '/',
+            redirect: '/home'
         },
 
 
-       
+
     ],
 
-    scrollBehavior () {
-        return{
-            left:0,
-            top:0
+    scrollBehavior() {
+        return {
+            left: 0,
+            top: 0
         }
     },
 
