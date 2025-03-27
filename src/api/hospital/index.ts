60a8a4948af6d14fb4079aa279ca59f71b3e99bd
saber <lhc@saber.com>
2025-03-27 15:02:47 +0800

    Update index.ts

--- src/api/hospital/index.ts
+++ src/api/hospital/index.ts
@@ -10,4 +10,5 @@ import type {HospitalDetail} from './type'
 enum API{
     HOSPITALDETAIL_URL = '/hosp/hospital/'
 }
+//获取医院详情的接口
 export const reqHospitalDetail = (hoscode: string) => request.get<any, HospitalDetail>(API.HOSPITALDETAIL_URL + hoscode);
