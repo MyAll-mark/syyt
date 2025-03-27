74694da927c49c8abf035f22de9d6d580a4654f3
saber <lhc@saber.com>
2025-03-27 20:59:56 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import '@/style/reset.scss'
-// import App from './App.vue'
+
 import App from '@/App.vue'
 //引入全局组件，都可以用
 import HospitalTop from '@/components/hospital_top/index.vue'
@@ -11,8 +11,13 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-// 引入仓库pinia
-import pinia from '@/store'
+// 引入仓库pinia  妈的傻逼，下面这个引入在这给我秀技术，老老实实引用不行吗？
+// import pinia from '@/store'
+//解决pinia
+import { createPinia } from 'pinia';
+const pinia = createPinia();
+
+
 
 
 const app=createApp(App);
@@ -29,4 +34,4 @@ app.use(router);
 app.use(pinia);
 //挂载
 app.mount('#app')
-// createApp(App).mount('#app')
+
