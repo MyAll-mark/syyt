8d54e951bb8f4aab718a0f1a007bd83a37dd7a4b
saber <lhc@saber.com>
2025-03-27 19:01:38 +0800

    Update hospitalDetails.ts

--- src/store/modules/hospitalDetails.ts
+++ src/store/modules/hospitalDetails.ts
@@ -1,11 +1,22 @@
 import { defineStore } from "pinia";
+import { reqHospitalDetail } from "@/api/hospital";
+import type { HospitalDetail } from "@/api/hospital/type";
 // 选择式API
 const useDetailStore = defineStore('Detail',{
     state:()=>{
-        return{}
+        return{
+            //医院详情的数据
+            hospitalInfo:{}
+        }
     },
     actions:{
-
+        async getHospital(hoscode:string){
+            let result : HospitalDetail =await reqHospitalDetail(hoscode)
+            if (result.code == 200){
+                this.hospitalInfo = result.data;
+            }
+            console.log("hoscode",hoscode)
+        }
     },
     getters:{
 
