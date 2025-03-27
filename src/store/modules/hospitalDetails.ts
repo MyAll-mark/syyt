06313cdafded2fe62f52bf221d759865340239db
saber <lhc@saber.com>
2025-03-27 14:45:20 +0800

    Create hospitalDetails.ts

---
+++ src/store/modules/hospitalDetails.ts
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+// 选择式API
+const useDetailStore = defineStore('Detail',{
+    state:()=>{
+        return{}
+    },
+    actions:{
+
+    },
+    getters:{
+
+    },
+});
+export default useDetailStore;
