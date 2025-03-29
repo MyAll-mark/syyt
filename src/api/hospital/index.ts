dfd9b02deaf8439bec91d2cd8cb75d1c3fb68eee
saber <lhc@saber.com>
2025-03-29 17:11:26 +0800

    Update index.ts

--- src/api/hospital/index.ts
+++ src/api/hospital/index.ts
@@ -1,6 +1,6 @@
 // 引入二次封装axios
 import request from "@/utils/request";
-import type {HospitalDetail} from './type'
+import type {HospitalDetail,DeparmentResponseData} from './type'
 // import type { DoctorInfoData,UserResponseData, 
 //     DoctorResponseData, HospitalWordData, 
 //     WXLoginResponseData, HospitalDetail, DeparmentResponseData,
@@ -8,7 +8,12 @@ import type {HospitalDetail} from './type'
 
 // 枚举请求地址
 enum API{
-    HOSPITALDETAIL_URL = '/hosp/hospital/'
+    HOSPITALDETAIL_URL = '/hosp/hospital/',
+    //获取某一个医院的科室的数据
+    HOSPITALDEPARMENT_URL = '/hosp/hospital/department/',
 }
 //获取医院详情的接口
 export const reqHospitalDetail = (hoscode: string) => request.get<any, HospitalDetail>(API.HOSPITALDETAIL_URL + hoscode);
+
+export const reqHospitalDeparment = (hoscode: string) => request.get<any,DeparmentResponseData>(API.HOSPITALDEPARMENT_URL + hoscode);
+
