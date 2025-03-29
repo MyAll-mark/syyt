0a53eb2dce8ec6ad18ca6d691388ad45fd68ac56
saber <lhc@saber.com>
2025-03-29 15:21:19 +0800

    Update index.vue

--- src/pages/hospital/close/index.vue
+++ src/pages/hospital/close/index.vue
@@ -1,16 +1,24 @@
 <template>
-    <div > 
-     停诊信息
+    <div>
+      <div class="info">
+        <h1>{{ hospitalStore.hospitalInfo.hospital?.hosname }}听诊信息</h1>
+        <el-empty description="暂无信息" />
+      </div>
     </div>
-</template>
-
-<script setup lang="ts">
-
-
-
-
-</script>
-
-<style lang='scss' scoped> 
-
-</style>
+  </template>
+  
+  <script setup lang="ts">
+  //引入医院详情仓库的数据
+  import useDetailStore from "@/store/modules/hospitalDetails";
+  let hospitalStore = useDetailStore();
+  </script>
+  
+  <style scoped lang="scss">
+  .info {
+    h1 {
+      text-align: center;
+      font-size: 30px;
+    }
+  }
+  </style>
+  
