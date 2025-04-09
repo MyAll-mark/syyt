8dcc0ba019b27b8e8445b092d3426ad6705329e3
saber <lhc@saber.com>
2025-04-09 14:56:12 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from '@/App.vue'
 //引入全局组件，都可以用
 import HospitalTop from '@/components/hospital_top/index.vue'
 import HospitalBottom from '@/components/hospital_bottom/index.vue'
+import login from '@/components/login/index.vue'
 //引入路由
 import router from './router'
 //引入element-plus插件
@@ -24,6 +25,7 @@ const app=createApp(App);
 
 app.component('HospitalTop',HospitalTop);
 app.component('HospitalBottom',HospitalBottom);
+app.component('login',login);
 //安装element-plus插件
 app.use(ElementPlus, {
     locale: zhCn,
