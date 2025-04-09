ccf10633c1f89518448355c0091e930f114704c6
saber <lhc@saber.com>
2025-04-09 14:56:15 +0800

    Create index.vue

---
+++ src/components/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login_container">
+        <el-dialog v-model="dialogFormVisible" title="用户登录"></el-dialog>
+    </div>
+</template>
+
+<script setup lang='ts'>
+// 导入 Vue 相关的 API
+import { ref } from 'vue';
+
+
+
+
+</script>
+
+<style lang='scss' scoped>
+/* 在这里添加组件的样式 */
+div {
+    
+}
+</style>
