0a3e5fa9e9498fafeed75b8a8a2d5e826cfe8ff7
saber <lhc@saber.com>
2025-04-09 15:36:42 +0800

    Update index.vue

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="login_container">
-        <el-dialog v-model="dialogFormVisible" title="用户登录"></el-dialog>
+        <el-dialog v-model="userStore.visiable" title="用户登录"></el-dialog>
     </div>
 </template>
 
 <script setup lang='ts'>
 // 导入 Vue 相关的 API
 import { ref } from 'vue';
+//获取user仓库的数据( visiable)可以控制login组件的对话框显示与隐藏
+import useUserStore from "@/store/modules/user";
+let userStore = useUserStore();
+
 
 
 
@@ -15,7 +19,62 @@ import { ref } from 'vue';
 
 <style lang='scss' scoped>
 /* 在这里添加组件的样式 */
-div {
-    
+.login_container {
+  ::v-deep(.el-dialog__body) {
+    border-top: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+  }
+
+  .login {
+    padding: 20px;
+    border: 1px solid #ccc;
+    .webchat {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .phone {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        p {
+          margin: 10px 0px;
+        }
+      }
+    }
+  }
+  .bottom {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    p {
+      margin: 10px 0px;
+    }
+  }
+
+  .leftContent {
+    .top {
+      display: flex;
+      justify-content: space-around;
+      .item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        img {
+          width: 130px;
+          height: 130px;
+        }
+        p {
+          margin: 5px 0px;
+        }
+      }
+    }
+  }
+
+  .tip {
+    text-align: center;
+    margin: 20px 0px;
+    font-size: 20px;
+    font-weight: 900;
+  }
 }
 </style>
