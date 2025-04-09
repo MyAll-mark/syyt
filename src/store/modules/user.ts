cce6e8b7c862d279d9f7f8f0f7674286e928f1e1
saber <lhc@saber.com>
2025-04-09 15:04:44 +0800

    Update user.ts

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,2 +1,19 @@
 import {defineStore} from "pinia"
 
+// 使用 Pinia 状态管理库来定义一个名为 User 的 store
+const useUserStore = defineStore("User",  {
+    state:()=>{
+        return{
+            visiable:false,//用于控制登录组件dialog显示与否的
+
+        }
+    },
+    actions:{
+
+    },
+
+    getters:{
+        
+    }
+
+} )
